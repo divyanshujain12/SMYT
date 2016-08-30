50b308c499378f3215f7d4b219c4d1642fe9b794
divyanshu.jain <divyanshujain12@hotmail.com>
2016-08-30 18:59:21 +0530

    office 30082016

---
+++ app/src/main/java/com/example/divyanshu/smyt/Fragments/PlaySingleVideoFragment.java
@@ -0,0 +1,47 @@
+package com.example.divyanshu.smyt.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.divyanshu.smyt.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by divyanshu.jain on 8/30/2016.
+ */
+public class PlaySingleVideoFragment extends DialogFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.all_video_fragment, null);
+        ButterKnife.inject(this, view);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initViews();
+    }
+
+    private void initViews() {
+
+    }
+}
