f902faa5f7dd8487e3eb062f217f7b0f5f5b8f2b
divyanshu.jain <divyanshujain12@hotmail.com>
2016-09-15 13:34:17 +0530

    office 15092016

---
+++ app/src/main/java/com/example/divyanshu/smyt/Constants/API.java
@@ -0,0 +1,14 @@
+package com.example.divyanshu.smyt.Constants;
+
+/**
+ * Created by divyanshu.jain on 9/15/2016.
+ */
+public interface API {
+
+    String BASE = "http://www.whatsupguys.in/demo/smyt/api/";
+    String REGISTRATION = BASE + "registration";
+    String LOGIN = BASE + "login";
+    String FORGOT_PASSWORD = BASE + "forgetpassword";
+    String GET_CATEGORIES = BASE + "getcategory";
+    String RESET_PASSWORD = BASE + "changepassword";
+}
