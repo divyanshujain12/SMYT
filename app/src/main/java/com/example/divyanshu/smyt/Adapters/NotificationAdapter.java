8e74e626ede515da4718795873ccf7c57642b23e
divyanshu.jain <divyanshujain12@hotmail.com>
2017-03-08 19:26:33 +0530

    notification tab integration

---
+++ app/src/main/java/com/example/divyanshu/smyt/Adapters/NotificationAdapter.java
@@ -0,0 +1,82 @@
+package com.example.divyanshu.smyt.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.divyanshu.smyt.CustomViews.RoundedImageView;
+import com.example.divyanshu.smyt.Interfaces.RecyclerViewClick;
+import com.example.divyanshu.smyt.Models.AllNotificationModel;
+import com.example.divyanshu.smyt.R;
+import com.example.divyanshu.smyt.Utils.ImageLoading;
+import com.example.divyanshu.smyt.Utils.Utils;
+import com.neopixl.pixlui.components.textview.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by divyanshu.jain on 3/8/2017.
+ */
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MyViewHolder> {
+
+    private ArrayList<AllNotificationModel> allNotificationModels;
+    private Context context;
+    private ImageLoading imageLoading;
+    private RecyclerViewClick recyclerViewClick;
+    private String UPCOMING_ROUND_TITLE_STRING = "Upcoming Round In %s";
+    private String UPCOMING_ROUND_DESC_STRING = "Your %snd round of %s start in next %s";
+
+    protected class MyViewHolder extends RecyclerView.ViewHolder {
+        private RoundedImageView userIV;
+        private TextView titleMsgTV, descTV, responseTV;
+
+        private MyViewHolder(View view) {
+            super(view);
+            userIV = (RoundedImageView) view.findViewById(R.id.userIV);
+            titleMsgTV = (TextView) view.findViewById(R.id.titleMsgTV);
+            descTV = (TextView) view.findViewById(R.id.descTV);
+            responseTV = (TextView) view.findViewById(R.id.responseTV);
+        }
+    }
+
+    public NotificationAdapter(Context context, ArrayList<AllNotificationModel> allNotificationModels, RecyclerViewClick recyclerViewClick) {
+        this.allNotificationModels = allNotificationModels;
+        this.context = context;
+        imageLoading = new ImageLoading(context);
+        this.recyclerViewClick = recyclerViewClick;
+    }
+
+    @Override
+    public NotificationAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.notification_rv_item, parent, false);
+
+        return new NotificationAdapter.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(NotificationAdapter.MyViewHolder holder, int position) {
+        AllNotificationModel allNotificationModel = allNotificationModels.get(position);
+        if (allNotificationModels.get(position).getStatus().equalsIgnoreCase("Inactive")) {
+            setUpViewForUpcomingChallenge(holder, allNotificationModel);
+        } else {
+
+        }
+    }
+
+    private void setUpViewForUpcomingChallenge(MyViewHolder holder, AllNotificationModel allNotificationModel) {
+        String timeDifference = Utils.getChallengeTimeDifference(allNotificationModel.getRound_date());
+        holder.titleMsgTV.setText(String.format(UPCOMING_ROUND_TITLE_STRING, timeDifference));
+        holder.descTV.setText(String.format(UPCOMING_ROUND_DESC_STRING, allNotificationModel.getRound_no(), allNotificationModel.getTitle(), timeDifference));
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+}
